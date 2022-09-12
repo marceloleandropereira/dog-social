e3ce569c6fad1077706d271db61677454a41d4b5
Marcelo Pereira <marceloleandropereira@gmail.com>
2022-09-12 14:41:02 -0300

    feat: new generic components and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Home from './Components/Home'
+import Login from './Components/Login/Login'
 
 const App = () => {
   return (
     <div>
-      App React
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
