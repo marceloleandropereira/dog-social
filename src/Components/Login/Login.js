1a79f5e487cac72cbf24c90d62de09985c4043dd
Marcelo Pereira <marceloleandropereira@gmail.com>
2022-09-13 11:18:21 -0300

    feat: add routes and components for login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LoginCreate from './LoginCreate'
+import LoginForm from './LoginForm'
+import LoginPasswordLost from './LoginPasswordLost'
+import LoginPasswordReset from './LoginPasswordReset'
 
 const Login = () => {
   return (
     <div>
-      Login
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/create" element={<LoginCreate />} />
+        <Route path="/lost" element={<LoginPasswordLost />} />
+        <Route path="/reset" element={<LoginPasswordReset />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LoginForm = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function handleLogin(event) {
+    event.preventDefault();
+    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then((json) => {
+      console.log(json);
+    })
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+
+      <form action="" onSubmit={handleLogin}>
+        <input 
+          type="text"
+          value={username}
+          onChange={({target}) => setUsername(target.value)}
+        />
+        <input 
+          type="password"
+          value={password}
+          onChange={({target}) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
+      </form>
+
+      <Link to="/login/create">Cadastro</Link>
+    </section>
+  )
+}
+
+export default LoginForm
